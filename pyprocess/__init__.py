28c63357d91850788fa36d352e358c615c91e759
Dieter J Kybelksties <github@kybelksties.com>
2026-03-12 18:30:10 +0000

    auto-committed by release-to-pypi.py

--- pyprocess/__init__.py
+++ pyprocess/__init__.py
@@ -1,5 +1,6 @@
-from .file_system_object import *
 from .file_utils import *
+from .__init__ import *
+from .file_system_object import *
 from .processes import *
 
-__version__ = "1.0.0"
+__version__ = "2.0.0"
